5c27d8ae89ee54bb62dc6be17bbc01404e1ee372
zack <arcanjo.fiec@gmail.com>
2022-11-17 00:00:25 +0000

    bot: 2022-11-17 00:00:25.565811

---
+++ differentialfunction.c
@@ -0,0 +1,26 @@
+
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ *
+ * Description: this function prints all arguments it receives
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
+
+
+
+
+"""@author: github.com/isacarcanjo"""
+
+
+
