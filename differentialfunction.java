b5e9754dcbb88ce767670b777b7b99d1ebe4b574
zack <arcanjo.fiec@gmail.com>
2022-11-19 00:00:22 +0000

    bot: 2022-11-19 00:00:22.850078

--- differentialfunction.java
+++ differentialfunction.java
@@ -40,3 +40,4 @@ public class file {
         double[] result = new double[values.length];
         for(int i = 0; i < values.length - n; i++) {
             for(int j = 0; j < n; j++) {
+                result[i] += values[i + j] * (n - j);
