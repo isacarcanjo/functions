5317f9e88aef88a8354a2b017c243661fb637219
zack <arcanjo.fiec@gmail.com>
2022-11-22 15:58:00 +0000

    bot: 2022-11-22 15:58:00.427735

---
+++ squarearea.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var r float64
+	fmt.Scan(&r)
+	area := math.Pow(r, 2)
+	fmt.Println(area)
+}
+
+
+
+
+"""@author: github.com/isacarcanjo"""
+
+
+
