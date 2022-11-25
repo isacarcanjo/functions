6fa0e9af4e929de4850cb45e2368a7b81b521848
zack <arcanjo.fiec@gmail.com>
2022-11-25 00:00:07 +0000

    bot: 2022-11-25 00:00:07.209555

---
+++ integralfunction.ts
@@ -0,0 +1,15 @@
+function integrate(coefficient, exponent){
+    let a = coefficient;
+    let b = exponent + 1;
+    let integral = (a/b);
+    return integral;
+}
+console.log(integrate(2, 2));
+
+
+
+
+"""@author: github.com/isacarcanjo"""
+
+
+
