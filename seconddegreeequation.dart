685172e4ffa1d8e45f46fb70a2afa222a79637b7
zack <arcanjo.fiec@gmail.com>
2022-11-30 00:00:30 +0000

    bot: 2022-11-30 00:00:30.164466

--- seconddegreeequation.dart
+++ seconddegreeequation.dart
@@ -7,3 +7,4 @@ Second Degree
 Instruksi
 =========
 Buatlah sebuah function yang menerima parameter dengan tipe data number bernama a, b, dan c.
+Function secondDegreeEquation akan menghitung akar dari persamaan kuadrat dengan parameter tersebut.
