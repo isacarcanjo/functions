95cb036a42a966fd321811f20ecf6c68afc1699b
zack <arcanjo.fiec@gmail.com>
2022-11-30 00:00:36 +0000

    bot: 2022-11-30 00:00:36.417052

--- seconddegreeequation.dart
+++ seconddegreeequation.dart
@@ -62,3 +62,4 @@ console.log(secondDegreeEquation(1, 2, 3)); // No Solution
 console.log(secondDegreeEquation(2, 5, -3)); // [ [ -1.5 ], [ 1 ] ]
 console.log(secondDegreeEquation(2, 4, 2)); // [ [ -1 ] ]
 console.log(secondDegreeEquation(4, 4, 1)); // [ [ -0.25, -1 ] ]
+
