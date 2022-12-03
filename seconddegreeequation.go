f26701306d0c67df9fd8f2d5e06d7ecffcf77b91
zack <arcanjo.fiec@gmail.com>
2022-12-03 00:00:27 +0000

    bot: 2022-12-03 00:00:27.721808

--- seconddegreeequation.go
+++ seconddegreeequation.go
@@ -47,3 +47,4 @@ func solve(a, b, c int) ([2]float64, error) {
 	}
 	//calculate the two solutions
 	x1 := (-float64(b) + math.Sqrt(Delta)) / 2 * float64(a)
+	x2 := (-float64(b) - math.Sqrt(Delta)) / 2 * float64(a)
