db4684275749811020d836632495a54ccf91ead7
zack <arcanjo.fiec@gmail.com>
2022-12-18 00:00:09 +0000

    bot: 2022-12-18 00:00:09.687726

--- integralfunction.dart
+++ integralfunction.dart
@@ -1,23 +1,2 @@
-num integralFunct(num a, num b, num n, num(num x) f){
-  num h = (b - a) / n;
-  num S = 0;
-  num x;
-  for (var i = 1; i <= n; i++){
-    x = a + (i - 0.5) * h;
-    S = S + f(x);
-  }
-  S = h * S;
-  return S;
-}
 
-main(){
-  print(integralFunct(1, 2, 4, (num x) => x*x));
-}
-
-
-
-
-"""@author: github.com/isacarcanjo"""
-
-
-
+//! Write a function in dart that calculates integral function
