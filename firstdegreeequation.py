b9229d70dc75b03d1c24002c0910276f616682f8
zack <arcanjo.fiec@gmail.com>
2022-12-19 00:00:07 +0000

    bot: 2022-12-19 00:00:07.653746

--- firstdegreeequation.py
+++ firstdegreeequation.py
@@ -1,21 +1,2 @@
-def solve_first_degree_equation(a, b, c):
-    """
-    Solves a first degree equation in the form:
-    a*x + b = c
-    """
 
-    x = (c - b) / a
-
-    return x
-
-
-r = solve_first_degree_equation(1, 2, 3)
-print(r)
-
-
-
-
-"""@author: github.com/isacarcanjo"""
-
-
-
+def equation(a,b):
