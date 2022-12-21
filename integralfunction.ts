fc8108e62766940bfb594d34f886221f1ffdb000
zack <arcanjo.fiec@gmail.com>
2022-12-21 00:00:07 +0000

    bot: 2022-12-21 00:00:07.698988

--- integralfunction.ts
+++ integralfunction.ts
@@ -1,12 +1,12 @@
-function integral(a:number,b:number,f:any) {
-    let sum=0;
-    for(let i=a;i<=b;i++){
-        sum+=f(i);
+function integral(func: (x:number)=>number, a:number, b:number, dx:number) {
+    let sum:number = 0;
+    for(let i = a; i < b; i+= dx) {
+        sum += (func(i)+func(i+dx))/2*dx;
     }
-    return sum;
+    console.log(sum);
 }
 
-console.log(integral(2,3,x=>x*x));
+integral(function(x) {return x*x}, 0, 1, 0.0001)
 
 
 
